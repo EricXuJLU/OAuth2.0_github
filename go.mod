c7de6d29171d38735a8c557a08296243962b56e6
xxn <xuxn2117@gmail.com>
2021-10-15 11:09:27 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module OAuth2.0_github
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1
+)
---
+++ showInfo/showinfo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("showInfo/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "intro.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8080")
+}
